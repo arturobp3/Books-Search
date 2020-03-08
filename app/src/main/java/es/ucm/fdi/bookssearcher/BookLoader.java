5d10a43183285f0e56c6391c1b70aff4adcf2ec2
Arturo <arbarber@ucm.es>
2020-03-08 22:00:32 +0100

    Se ha añadido el código para acceder a la API de Google. Aún está sin probar.

--- app/src/main/java/es/ucm/fdi/bookssearcher/BookLoader.java
+++ app/src/main/java/es/ucm/fdi/bookssearcher/BookLoader.java
@@ -1,24 +1,74 @@
 package es.ucm.fdi.bookssearcher;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.loader.content.AsyncTaskLoader;
 
+import org.json.JSONObject;
+
+import java.util.List;
+
 public class BookLoader extends AsyncTaskLoader<String> {
+
+    private static final String DEBUG_TAG = BookLoader.class.getSimpleName();
 
     private String mQueryString;
     private BookService bookService;
+    private Context context;
 
     public BookLoader(Context context, String queryString) {
         super(context);
+        this.context = context;
         mQueryString = queryString;
+        bookService = new BookService();
     }
+
+    private boolean internetConnectionAvailable(){
+
+        ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        /*NetworkInfo networkInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        boolean isWifiConn = networkInfo.isConnected();
+        networkInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        boolean isMobileConn = networkInfo.isConnected();*/
+
+        /*Log.d(DEBUG_TAG, "Wifi connected: " + isWifiConn);
+        Log.d(DEBUG_TAG, "Mobile connected: " + isMobileConn);*/
+        //return (isMobileConn || isWifiConn);
+
+        // AL DESCOMENTAR ESAS INSTRUCCIONES DE ARRIBA, PONE QUE NO SE PUEDE CON LA API 26 DE ANDROID
+
+
+        NetworkInfo activeNetwork = connMgr.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnected();
+
+        return isConnected;
+    }
+
 
     @Nullable
     public String loadInBackground(String queryString, String printType) {
 
-        json = bookService.getBookInfoJson(queryString, printType);
+        if (internetConnectionAvailable()) {
+            JSONObject json = bookService.getBookInfoJson(queryString, printType);
+            if(json != null){
+
+                // ASI SE PUEDE ACCEDER A LOS DATOS
+
+                /*JSONArray menuItemArray = data.getJSONArray("menuitem");
+                JSONObject thirdItem = menuItemArray.getJSONObject(2);
+                String onClick = thirdItem.getString("onclick");*/
+            } else{
+
+
+            }
+        } else{
+
+        }
 
 
         return "Hola";
--- app/src/main/java/es/ucm/fdi/bookssearcher/BookService.java
+++ app/src/main/java/es/ucm/fdi/bookssearcher/BookService.java
@@ -1,13 +1,100 @@
 package es.ucm.fdi.bookssearcher;
 
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class BookService {
 
-    public JSONObject getBookInfoJson(String queryString, String printType){
+    private static final String DEBUG_TAG = BookService.class.getSimpleName();;
+    private final String BOOK_VOLUME_BASE_URL = "https://www.googleapis.com/books/v1/volumes?";
 
-        
+    // Reads an InputStream and converts it to a String.
+    public String convertInputToString(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
 
+
+    private String createURL(String queryString, String printType, String baseURL){
+        // Parameter for the search string
+        final String QUERY_PARAM = "q";
+        // Parameter to limit search results.
+        final String MAX_RESULTS = "maxResults";
+        // Parameter to filter by print type
+        final String PRINT_TYPE = "printType";
+
+        // Build up the query URI, limiting results to 5 printed books.
+        Uri builtURI = Uri.parse(baseURL).buildUpon()
+                .appendQueryParameter(QUERY_PARAM, queryString)
+                .appendQueryParameter(MAX_RESULTS, "5")
+                .appendQueryParameter(PRINT_TYPE, printType)
+                .build();
+
+        return builtURI.toString();
+    }
+
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream inputStream = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+        HttpURLConnection conn = null;
+        try {
+            URL url = new URL(myurl);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            // Start the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d(DEBUG_TAG, "The response is: " + response);
+            inputStream = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = convertInputToString(inputStream, len);
+            return contentAsString;
+
+            // Close the InputStream and connection
+        } finally {
+            conn.disconnect();
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+    }
+
+
+    public JSONObject getBookInfoJson(String queryString, String printType) {
+
+        String url = this.createURL(queryString, printType, this.BOOK_VOLUME_BASE_URL);
+        JSONObject data = null;
+        try {
+            String info = this.downloadUrl(url);
+            data = new JSONObject(info);
+
+            System.out.println(data.toString());
+
+
+        } catch (IOException | JSONException e) {
+            System.out.println("Ha habido un error en BookService: " + e.getStackTrace());
+        }
+
+        return data;
     }
 
 
